eb8239afc60a90a58c266529d86cf735cbe944bd
Noah Halstead <nhalstead@gmail.com>
2017-11-01 12:30:44 -0400

    Added minHeight & minWidth

--- electronTesting/main.js
+++ electronTesting/main.js
@@ -15,7 +15,8 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 800, height: 600, 
+							minWidth: 800, minHeight: 600})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
