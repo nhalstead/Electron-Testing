73e3ae541feb98bff25e932be1c8b3998d64043d
Noah Halstead <nhalstead@gmail.com>
2017-11-01 22:29:51 -0400

    Cleaned up the Code and Added some Tray Code

--- electronTesting/main.js
+++ electronTesting/main.js
@@ -1,6 +1,8 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu, Tray} = require('electron')
 const path = require('path')
 const url = require('url')
+var fs = require('fs')
+var events = require('events')
 
 // https://electron.atom.io/docs/tutorial/quick-start/
 // https://github.com/electron/electron/tree/master/docs/tutorial
@@ -11,12 +13,19 @@ const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+	let win
+	const iconPath = path.join(__dirname, 'icon.png');
+	let appIcon = null;
+// Console Log the Args
+	var arguments = process.argv.slice(2);
+	arguments.forEach(function(val,index, array) {
+		console.log(index + ': ' + val);
+	});
+//Disable all of the Default Menus
+	app.on('browser-window-created',function(e,window) {
+	  window.setMenu(null);
+	});
 
-var arguments = process.argv.slice(2);
-arguments.forEach(function(val,index, array) {
-	console.log(index + ': ' + val);
-}); 
 
 // https://electron.atom.io/docs/api/browser-window/
 function createWindow () {
@@ -37,17 +46,12 @@ function createWindow () {
 		webSecurity: true
 	})
   
-  //win.setMenu(null);
-  
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
-
-  // Open the DevTools.
-  //win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
@@ -61,12 +65,13 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', function(){
+	createWindow();
+	setupTrayIcon();
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
@@ -91,5 +96,40 @@ app.setUserTasks([
   }
 ])
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+function setupTrayIcon (){
+	// Setup the Tray Icon
+	appIcon = new Tray(iconPath);
+	var contextMenu = Menu.buildFromTemplate([
+	{
+		label: 'Bring Forward',
+		click: function(){
+			win.show();
+		}
+	},
+	{
+	  label: 'Menu',
+	  submenu: [
+		{ label: 'Sub Menu 1' },
+		{ label: 'Sub Menu 2' }
+	  ]
+	},
+	{
+	  label: 'Toggle DevTools',
+	  click: function() {
+		win.show();
+		win.toggleDevTools();
+	  }
+	},
+	{ label: 'Quit',
+	  accelerator: 'Command+Q',
+	  click: function() {
+		win = null;
+		if (process.platform !== 'darwin') {
+			app.quit();
+		}
+	  }
+	}
+	]);
+	appIcon.setToolTip('This is my application.');
+	appIcon.setContextMenu(contextMenu);
+}
